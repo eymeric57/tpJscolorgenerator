bc414bdcc70e3d75206cac8f18a0599597af8055
unknown <eymeric57160@hotmail.fr>
2023-11-06 15:41:55 +0100

    finished tp

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const rgbspan = document.getElementById("rgbtext");
 
-const body = document.body;
-const color1 = document.getElementById("rgb1")
+const rgbc1 = document.getElementById("rgb1");
+const rgbc2 = document.getElementById("rgb2");
+const rgbc3 = document.getElementById("rgb3");
 
+const body = document.body;
 
 setInterval(() => {
-    let rgb1 = Math.floor(Math.random() * 255);
-    let rgb2 = Math.floor(Math.random() * 255);
-    let rgb3 = Math.floor(Math.random() * 255);
-    
+  let rgb1 = Math.floor(Math.random() * 255);
+  let rgb2 = Math.floor(Math.random() * 255);
+  let rgb3 = Math.floor(Math.random() * 255);
 
-    
+  rgbc1.innerHTML = ` ${rgb1},`;
+  rgbc2.innerHTML = ` ${rgb2},`;
+  rgbc3.innerHTML = ` ${rgb3}`;
+
   body.style.background = "rgb(" + rgb1 + "," + rgb2 + "," + rgb3 + ")";
 
-  rgbspan.innerHTML = `<h3>RGB: ${rgb1}, ${rgb2}, ${rgb3} </h3>`;
+  rgbc1.style.color = "#" + rgb1;
+  rgbc2.style.color = "#" + rgb2;
+  rgbc3.style.color = "#" + rgb3;
 }, 1000);
-
-
